72c82cc4832b397c35f378e16084135537bb16de
WebApp64 <tone@saraya.com>
2013-04-05 15:00:56 +0900

    Update #4　comps-emps-slaray report 追加

--- Contacts/Model.js
+++ Contacts/Model.js
@@ -1,4 +1,22 @@
 ﻿
 guidedModel =// @startlock
 {
+	Employee :
+	{
+		fullName :
+		{
+			onGet:function()
+			{// @endlock
+			return this.lastName+','+this.firstName;
+			}// @startlock
+		},
+		collectionMethods :
+		{// @endlock
+			myReport:function()
+			{// @lock
+			var res = this.compute("salary","company,fullName"); 
+    		return res.toArray(); 				// Add your code here
+			}// @startlock
+		}
+	}
 };// @endlock
---
+++ Contacts/WebFolder/comps-emps-salarys.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var MakeReport = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	MakeReport.click = function MakeReport_click (event)// @startlock
+	{// @endlock
+        report = sources.employee.myReport(); // call the class method and put the result in the array
+        sources.report.sync(); //synchronize the report array with the report datasource
+
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("MakeReport", "click", MakeReport.click, "WAF");
+// @endregion
+};// @endlock
